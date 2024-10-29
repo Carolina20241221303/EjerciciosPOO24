1494f0de90afd453acb34c0eccbd6e77130c90cd
Carolina20241221303 <u20241221303@usco.edu.co>
2024-10-29 16:58:35 -0500

    Create forma.java

---
+++ forma.java
@@ -0,0 +1,51 @@
+public class forma {
+
+    public String nombre;
+    public double posicionX;
+    public double posicionY;
+    public String color;
+
+    public forma(String nombre, double posicionX, double posicionY, String color) {
+        this.nombre = nombre;
+        this.posicionX = posicionX;
+        this.posicionY = posicionY;
+        this.color = color;
+    }
+
+    public double calcularArea() {
+        return 0;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPosicionX() {
+        return posicionX;
+    }
+
+    public void setPosicionX(double posicionX) {
+        this.posicionX = posicionX;
+    }
+
+    public double getPosicionY() {
+        return posicionY;
+    }
+
+    public void setPosicionY(double posicionY) {
+        this.posicionY = posicionY;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+}
